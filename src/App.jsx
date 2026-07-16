52ad835c72056e2d36a0cc8833d2a97a23910619
Sanjay-Makhija <sanjay.makhija@mikadosolutions.com>
2026-07-17 00:13:01 +0530

    Commit ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import ExportCenter from "./pages/ExportCenter";
+import CollectionDetails from "./pages/CollectionDetails";
+import Favorites from "./pages/Favorites";
+import { DashboardPage } from "./pages/DashboardPage";
+import { DiscoveryPage } from "./pages/DiscoveryPage";
 function App() {
-
+    return( 
+        <DiscoveryPage/>
+    );
 }
 
 export default App;
